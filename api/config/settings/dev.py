bcf816e1a5b505fa08cf7f9f71d61105a915d341
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2024-07-18 18:39:06 +0700

    example env
    example env

--- api/config/settings/dev.py
+++
@@ -1,7 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&mjja5lhg1rt4ex3ysj8v3#=v0p4lwoeffckr)82!qqs_*3nzj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
